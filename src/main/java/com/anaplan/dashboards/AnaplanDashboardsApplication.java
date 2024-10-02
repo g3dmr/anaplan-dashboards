8a59185d06f3c88d103f28d3cf6151f8d321d815
Mohanraj Chinnaswamy <sgaya3devi@hotmail.com>
2024-10-02 08:04:20 +0100

    Initial commit

---
+++ src/main/java/com/anaplan/dashboards/AnaplanDashboardsApplication.java
@@ -0,0 +1,13 @@
+package com.anaplan.dashboards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnaplanDashboardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnaplanDashboardsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anaplan/dashboards/AnaplanDashboardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anaplan.dashboards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnaplanDashboardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
